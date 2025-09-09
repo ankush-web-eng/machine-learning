2731df10605b4028d18f3ce491ea0195608bbf7d
Ankush <deshwalankush23@gmail.com>
2025-09-09 10:22:32 +0530

    Encoding done

--- pre-processing/main.py
+++ pre-processing/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
 
 dataset = pd.read_csv('data.csv')
 
@@ -10,9 +9,27 @@ print(empty_cols)
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(X=x))
 print(x)
+
+le = LabelEncoder()
+y = le.fit_transform(y=y)
 print(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=1)
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
